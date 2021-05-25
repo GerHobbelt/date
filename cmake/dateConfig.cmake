ad2a76253774cda9f0a8e02975386b2afee1c991
Dausel, Martin <Martin.Dausel@barco.com>
2021-05-25 15:42:41 +0200

    Provide mechanism to locate tzdata under specified path instead fixed install location

--- cmake/dateConfig.cmake
+++ cmake/dateConfig.cmake
@@ -1,8 +1,14 @@
 include( CMakeFindDependencyMacro )
+
+# Importing battzdata::battzdata via find_dependency(BatTzData) is not needed
+# because the importet target does not contain any usage infos. 
+# battzdata is only interesting, when building date-tz. 
+# The target battzdata::battzdata must exists anyway because the generated 
+# dateTargets.cmake  will reference it as dependency.
+# So lets create a dummy:
+add_library( battzdata::battzdata INTERFACE IMPORTED)
+
 include( "${CMAKE_CURRENT_LIST_DIR}/dateTargets.cmake" )
-if( TARGET date::date-tz ) # fix that
-    find_dependency( BatTzdata REQUIRED)    
-endif()
 
 if( NOT MSVC AND TARGET date::date-tz )
     find_dependency( Threads REQUIRED)
@@ -12,4 +18,6 @@ if( NOT MSVC AND TARGET date::date-tz )
     endif( )
 endif( )
 
-
+#if (TARGET date::date-tz AND @USE_BAT_TZ_DB@)
+#    find_dependency( BatTzdata REQUIRED)
+#endif()
