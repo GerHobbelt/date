dbb4ef084d88377e4cae56b70bf9bf278b709f73
Rob Houston <rhouston@esri.com>
2024-09-03 16:36:56 +0100

    further stl fixes

--- include/date/tz.h
+++ include/date/tz.h
@@ -263,11 +263,11 @@ ambiguous_local_time::make_msg(local_time<Duration> tp, const local_info& i)
 {
     assert(i.result == local_info::ambiguous);
     std::ostringstream os;
-    os << tp << " is ambiguous.  It could be\n"
-       << tp << ' ' << i.first.abbrev << " == "
-       << tp - i.first.offset << " UTC or\n"
-       << tp << ' ' << i.second.abbrev  << " == "
-       << tp - i.second.offset  << " UTC";
+    date::operator<<(os, tp) << " is ambiguous.  It could be\n";
+    date::operator<<(os, tp) << ' ' << i.first.abbrev << " == ";
+    date::operator<<(os, tp - i.first.offset) << " UTC or\n";
+    date::operator<<(os, tp) << ' ' << i.second.abbrev << " == ";
+    date::operator<<(os, tp - i.second.offset) << " UTC";;
     return os.str();
 }
 
